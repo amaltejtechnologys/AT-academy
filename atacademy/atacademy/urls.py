7931e1ece68a25a79d8790168a5edffc0dbce241
Ayaan <ayaan2008.admas@gmail.com>
2026-07-07 10:55:12 +0530

    Fix media files not loading in production: always serve media regardless of DEBUG

--- atacademy/atacademy/urls.py
+++ atacademy/atacademy/urls.py
@@ -1,12 +1,18 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve as static_serve
 from core.admin import admin_site
 
 urlpatterns = [
     path('admin/', admin_site.urls),
     path('', include('core.urls')),
+]
+
+# Always serve media files (uploaded images) regardless of DEBUG
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', static_serve, {'document_root': settings.MEDIA_ROOT}),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
