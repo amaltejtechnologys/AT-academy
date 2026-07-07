9474404433238d97e5936a944858b4cef91b32d1
Ayaan <ayaan2008.admas@gmail.com>
2026-07-07 12:02:36 +0530

    Split migration: 0005 has existing tables, 0006 adds CourseBrochure only

--- atacademy/core/migrations/0005_all_additions.py
+++ atacademy/core/migrations/0005_all_additions.py
@@ -35,18 +35,4 @@ class Migration(migrations.Migration):
                 'ordering': ['-created_at'],
             },
         ),
-        migrations.CreateModel(
-            name='CourseBrochure',
-            fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('title', models.CharField(help_text='e.g. Full Stack Python Syllabus 2026', max_length=200)),
-                ('file', models.FileField(help_text='PDF brochure', upload_to='brochures/')),
-                ('order', models.IntegerField(default=0)),
-                ('course', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='brochures', to='core.course')),
-            ],
-            options={
-                'verbose_name_plural': 'Course Brochures',
-                'ordering': ['order'],
-            },
-        ),
     ]
